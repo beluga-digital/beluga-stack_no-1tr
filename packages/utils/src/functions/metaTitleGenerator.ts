fe33c0a3c62bd2e131b75dd61fce023f594d68af
beluga-digital <d.baumert96@gmail.com>
2024-08-12 12:30:55 +0200

    add metaTitleGenerator utility function

---
+++ packages/utils/src/functions/metaTitleGenerator.ts
@@ -0,0 +1,32 @@
+export const META_TITLE_APP_NAME = "beluga stack no.1 turborepo";
+export const META_TITLE_APP_NAME_DIVIDER = " | ";
+export const META_TITLE_APP_NAME_POSITION = "end"; // start | end
+export const META_TITLE_PAGES_DIVIDER = " – ";
+
+export const metaTitleGenerator = (
+  pages: string[],
+  options?: {
+    appName?: string;
+    appNameDivider?: string;
+    appNamePosition?: "start" | "end";
+    pagesDivider?: string;
+  }
+): string => {
+  const appName = options?.appName ?? META_TITLE_APP_NAME;
+  const appNameDivider = options?.appNameDivider ?? META_TITLE_APP_NAME_DIVIDER;
+  const appNamePosition =
+    options?.appNamePosition ?? META_TITLE_APP_NAME_POSITION;
+  const pagesDivider = options?.pagesDivider ?? META_TITLE_PAGES_DIVIDER;
+
+  const pagesString = pages.join(pagesDivider);
+
+  if (appNamePosition == "start") {
+    return appName + appNameDivider + pagesString;
+  }
+
+  if (appNamePosition == "end") {
+    return pagesString + appNameDivider + appName;
+  }
+
+  throw Error("Error executing metaTitleGenerator.");
+};
