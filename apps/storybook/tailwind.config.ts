c25c7e3a8e35e5c2f146728e6853dd0ab25739c3
beluga-digital <d.baumert96@gmail.com>
2024-08-14 23:56:08 +0200

    integrate storybook as separate app

---
+++ apps/storybook/tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind config is required for editor support
+
+import sharedConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss";
+
+const config: Pick<Config, "prefix" | "content" | "presets"> = {
+  presets: [sharedConfig],
+  content: ["../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"],
+};
+
+export default config;
