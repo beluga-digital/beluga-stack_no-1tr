551c922205fcb5303c1cf6e0ca4d2793b34b9c94
beluga-digital <d.baumert96@gmail.com>
2024-08-15 22:39:07 +0200

    add tailwind darkmode

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,14 +1,15 @@
-import "../src/globals.css";
 import "../../../packages/ui/dist/styles/globals.css";
-import { withThemeByClassName } from "@storybook/addon-themes";
+import "../src/globals.css";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
 
 export const decorators = [
-  withThemeByClassName({
+  withThemeByDataAttribute({
     themes: {
       light: "light",
       dark: "dark",
     },
     defaultTheme: "light",
+    attributeName: "data-mode",
   }),
 ];
 
@@ -16,6 +17,16 @@ export const decorators = [
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      disable: true,
+      grid: {
+        cellSize: 20,
+        opacity: 0.5,
+        cellAmount: 5,
+        offsetX: 16, // Default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
+        offsetY: 16, // Default is 0 if story has 'fullscreen' layout, 16 if layout is 'padded'
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
